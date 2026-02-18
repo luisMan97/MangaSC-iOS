80a3a5619fed4c604d392825e4e271aafaddd71e
Jorge Luis Rivera Ladino <riveraladinojorgeluis@gmail.com>
2026-02-18 15:04:09 -0500

    Initial Commit

---
+++ Manga-iOS/Manga_iOSApp.swift
@@ -0,0 +1,32 @@
+//
+//  Manga_iOSApp.swift
+//  Manga-iOS
+//
+//  Created by Jorge Luis Rivera on 18/02/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Manga_iOSApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ Manga-iOSTests/Manga_iOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  Manga_iOSTests.swift
+//  Manga-iOSTests
+//
+//  Created by Jorge Luis Rivera on 18/02/26.
+//
+
+import Testing
+@testable import Manga_iOS
+
+struct Manga_iOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
